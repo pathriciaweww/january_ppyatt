59969cec860e7647b59a71bf86fb586413b2157e
pathriciaweww <pathriciawe@gmail.com>
2023-01-28 23:00:14 +1300

    complete

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import './features.css';
 
+import { Feature } from '../../components';
+
+const featuresData =[
+  {
+    tittle:'Improving end distrusts instantly',
+    text:'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+  },
+  {
+    tittle:'Become the tended active',
+    text:'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.'
+  },
+  {
+    tittle:'Message or am nothing',
+    text:'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.'
+  },
+  {
+    tittle:'Really boy law county',
+    text:'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.'
+  }
+]
+
 const Features = () => {
   return (
-    <div className='gpt3__features section__margin' id='features'>
+    <div className='gpt3__features section__padding' id='features'>
       <div className='gpt3__features-heading'>
         <h1 className='gradient__text'>
           The Future is Now and You Just Need To Realize It. Step into Future Today &amp; Make it Happen.
@@ -14,10 +35,9 @@ const Features = () => {
       </div>
 
       <div className='gpt3__features-container'>
-        <Features title="Improving end distrusts instantly" text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."/>
-        <Features title="Become the tended active" text="Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."/>
-        <Features title="Message or am nothing" text="Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."/>
-        <Features title="Really boy law county" text="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."/>
+        {featuresData.map((item, index) => (
+          <Feature title={item.tittle} text={item.text} key={item.tittle + index}/>
+        ))}
       </div>
     </div>
   )
--- src/containers/whatGPT/WhatGPT.jsx
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -18,8 +18,8 @@ const WhatGPT = () => {
       </div>
       <div className='gpt3__whatgpt3-container'>
           <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "/>
-          <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
-          <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
+          <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"/>
+          <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"/>
       </div>
     </div>
   )
